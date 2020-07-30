0c533dd3701cfe4db81f3cfaf0281fd473aa54ca
MayankGupta <mayank.gupta.6.88@gmail.com>
2020-07-30 17:14:08 +0530

    changes

--- pyQT/SampleOne.py
+++ pyQT/SampleOne.py
@@ -2,6 +2,8 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QLineEdit
 
 b2 = None
+b1 = None
+b3 = None
 
 def window():
    app = QApplication([])
@@ -15,18 +17,24 @@ def window():
    b2.setText("Button2")
    b2.move(100,100)
    b2.clicked.connect(b1_clicked)
-
+   
    window.setWindowTitle("PyQt")
 
+   global b3
    b3 = QLineEdit(window)
    b3.setText("Button2")
-   b3.move(200,200)
+   b3.move(20,200)
+
+   global b1
+   b1 = QLineEdit(window)
+   b1.setText("Button1")
+   b1.move(200,200)
 
    window.show()
    sys.exit(app.exec_())
 
 def b1_clicked():
-    b2.setText("Clicked Button...")
+    b2.setText(str(int(b1.text()) + int(b3.text())))
 	
 window()
 
